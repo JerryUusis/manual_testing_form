f3c8c5e083f4d1e49a29fed29f32569ed70c89c5
Jerry Uusitalo <jerry.uusitalo@gmail.com>
2025-01-27 16:14:16 +0200

    Install dependencies and remove boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    exclude: ["./__tests__/e2e/**", "./node_modules"],
+    globals: true,
+    coverage: {
+      include: ["src"]
+    }
+  },
+});
